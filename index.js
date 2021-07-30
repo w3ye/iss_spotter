f6a9899f47616005407e4f60d82eff7ce71bd3da
William Ye <w3ye@pm.me>
2021-07-29 23:51:55 -0400

    index.js now only runs nextISSTimesForMyLocation

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-const {fetchMyIP,
-       fetchCoordsByIP,
-       fetchISSFlyOverTimes
-      } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-
-  fetchCoordsByIP(ip, (err, coord) => {
-    if (err) return console.log(err);
-
-    fetchISSFlyOverTimes(coord, (err, data) => {
-      if (err) return console.log(err);
-
-      console.log(data);
-    })
-  });
+nextISSTimesForMyLocation((err, passTime) => {
+  if (err) return console.log("It didn't work!", err);
+  console.log(passTime);
 });
